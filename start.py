c4d1579a97ad5dfdbcedca6db38a94cab5f7b644
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-08-13 11:34:06 -0500

    Got rid of silly newlines

--- start.py
+++ start.py
@@ -5,6 +5,12 @@
 def main():
     inputFile = open("test.js", "r+")
     for line in inputFile:
-        print line
+        lnToPrint = ""
+        for character in line:
+            # Make sure this isn't a silly newline, if it isn't, we can print this character.
+            if character != "\n":
+                lnToPrint += character
+
+        print lnToPrint
 
 main()
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 var test = function() {
     console.log("Hello, world!");
+    
+    console.log("This is a test file!");
 };
