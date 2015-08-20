a3420a76916490945e56d2f2c6b896e81f676b7a
JavascriptFTW <joshuamagma2013@gmail.com>
2015-08-20 15:05:46 -0500

    Minor changes
    Minor changes. Got rid of the stripNewLines method and replaced it with
    the native replace string method, renamed lint to lintLine as the naming
    may have been deceptive, as well as adding a gitignore file.

--- Javascript.py
+++ Javascript.py
@@ -1,37 +1,35 @@
+
 class JavascriptLinter:
     version = "1.0"
     inputFile = None
     filePointer = None
     lines = [ ]
+    errors = [ ]
+    warnings = [ ]
+    style = [ ]
 
     def __init__(self, inputFile):
-        print "JavascriptLinter v" + self.version
-        print "File to lint: \"" + inputFile + "\""
+        print ("JavascriptLinter version " + self.version)
+        print ("File to lint: \"" + inputFile + "\"")
         self.inputFile = inputFile
         self.filePointer = open(self.inputFile, "r+")
         self.lines = self.getLines()
 
-
-    def stripNewlines(self, line):
-        retLine = ""
-        for character in line:
-            if character != "\n":
-                retLine += character
-        return retLine
-
     def getLines(self):
         lines = []
         for line in self.filePointer:
-            lines.append(self.stripNewlines(line))
+            lines.append(line.replace("\n", ""))
+
+        print ("There are " + str(len(lines)) + " lines in " + str(self.inputFile))
 
         return lines
 
-    def lint(self, line):
+    def lintLine(self, line):
         if str(line).find("console") != -1:
-            print "I saw console!"
+            print ("I saw console!")
 
     def lintFile(self):
         for line in self.lines:
-            self.lint(line)
+            self.lintLine(line)
 
-JavascriptLinter("test.js");
+JavascriptLinter("test.js")
